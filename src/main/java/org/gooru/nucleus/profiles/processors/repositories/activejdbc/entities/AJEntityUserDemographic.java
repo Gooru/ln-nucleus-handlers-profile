cc528af922d300cc083c6aa5e597d4c4da797e51
sachinszope <sachin@goorulearning.org>
2016-03-14 17:46:31 +0530

    changes to get school district to be displayed on user card on profile

--- src/main/java/org/gooru/nucleus/profiles/processors/repositories/activejdbc/entities/AJEntityUserDemographic.java
+++ src/main/java/org/gooru/nucleus/profiles/processors/repositories/activejdbc/entities/AJEntityUserDemographic.java
@@ -13,11 +13,12 @@ public class AJEntityUserDemographic extends Model {
   public static final String FIRSTNAME = "firstname";
   public static final String LASTNAME = "lastname";
   public static final String TEMPLATE_PATH = "thumbnail_path";
+  public static final String SCHOOL_DISTRICT_ID = "school_district_id";
   
   public static final String SELECT_DEMOGRAPHICS = "SELECT id, firstname, lastname, thumbnail_path FROM user_demographic WHERE id = ?::uuid";
-  public static final String SELECT_DEMOGRAPHICS_MULTIPLE = "SELECT id, firstname, lastname, thumbnail_path FROM user_demographic WHERE id ="
-    + " ANY (?::uuid[])";
+  public static final String SELECT_DEMOGRAPHICS_MULTIPLE = "SELECT id, firstname, lastname, thumbnail_path, school_district_id FROM user_demographic"
+    + " WHERE id = ANY (?::uuid[])";
   
-  public static final List<String> DEMOGRAPHIC_FIELDS = Arrays.asList(ID, FIRSTNAME, LASTNAME, TEMPLATE_PATH);
+  public static final List<String> DEMOGRAPHIC_FIELDS = Arrays.asList(ID, FIRSTNAME, LASTNAME, TEMPLATE_PATH, SCHOOL_DISTRICT_ID);
 
 }
