7e420b7237add93c490e6e22fd2ee65e59b82569
am-gooru <ashish@gooru.org>
2016-05-09 18:30:25 +0530

    Startup hardening

--- src/main/java/org/gooru/nucleus/handlers/profiles/bootstrap/ProfileVerticle.java
+++ src/main/java/org/gooru/nucleus/handlers/profiles/bootstrap/ProfileVerticle.java
@@ -1,10 +1,5 @@
 package org.gooru.nucleus.handlers.profiles.bootstrap;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
-import io.vertx.core.eventbus.EventBus;
-import io.vertx.core.json.JsonObject;
-
 import org.gooru.nucleus.handlers.profiles.bootstrap.shutdown.Finalizer;
 import org.gooru.nucleus.handlers.profiles.bootstrap.shutdown.Finalizers;
 import org.gooru.nucleus.handlers.profiles.bootstrap.startup.Initializer;
@@ -16,6 +11,11 @@ import org.gooru.nucleus.handlers.profiles.processors.responses.MessageResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.json.JsonObject;
+
 public class ProfileVerticle extends AbstractVerticle {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ProfileVerticle.class);
@@ -23,51 +23,49 @@ public class ProfileVerticle extends AbstractVerticle {
     @Override
     public void start(Future<Void> voidFuture) throws Exception {
 
+        EventBus eb = vertx.eventBus();
+
         vertx.executeBlocking(blockingFuture -> {
             startApplication();
             blockingFuture.complete();
-        } , future -> {
-            if (future.succeeded()) {
-                voidFuture.complete();
+        }, startApplicationFuture -> {
+            if (startApplicationFuture.succeeded()) {
+                eb.consumer(MessagebusEndpoints.MBEP_PROFILE, message -> {
+                    LOGGER.debug("Received message: " + message.body());
+                    vertx.executeBlocking(future -> {
+                        MessageResponse result = new ProcessorBuilder(message).build().process();
+                        LOGGER.info("got response :" + result.reply());
+                        future.complete(result);
+                    }, res -> {
+                        MessageResponse result = (MessageResponse) res.result();
+                        message.reply(result.reply(), result.deliveryOptions());
+                        JsonObject eventData = result.event();
+                        if (eventData != null) {
+                            String sessionToken =
+                                ((JsonObject) message.body()).getString(MessageConstants.MSG_HEADER_TOKEN);
+                            if (sessionToken != null && !sessionToken.isEmpty()) {
+                                eventData.put(MessageConstants.MSG_HEADER_TOKEN, sessionToken);
+                            } else {
+                                LOGGER.warn("Invalid session token received");
+                            }
+                            eb.publish(MessagebusEndpoints.MBEP_EVENT, eventData);
+                        }
+                    });
+                }).completionHandler(result -> {
+                    if (result.succeeded()) {
+                        voidFuture.complete();
+                        LOGGER.info("Profile end point ready to listen");
+                    } else {
+                        LOGGER.error("Error registering the profile handler. Halting the Profile machinery");
+                        voidFuture.fail(result.cause());
+                        Runtime.getRuntime().halt(1);
+                    }
+                });
             } else {
                 voidFuture.fail("Not able to initialize the Profile machinery properly");
             }
         });
 
-        EventBus eb = vertx.eventBus();
-
-        eb.consumer(MessagebusEndpoints.MBEP_PROFILE, message -> {
-
-            LOGGER.debug("Received message: " + message.body());
-
-            vertx.executeBlocking(future -> {
-                MessageResponse result = new ProcessorBuilder(message).build().process();
-                LOGGER.info("got response :" + result.reply());
-                future.complete(result);
-            } , res -> {
-                MessageResponse result = (MessageResponse) res.result();
-                message.reply(result.reply(), result.deliveryOptions());
-
-                JsonObject eventData = result.event();
-                if (eventData != null) {
-                    String sessionToken = ((JsonObject) message.body()).getString(MessageConstants.MSG_HEADER_TOKEN);
-                    if (sessionToken != null && !sessionToken.isEmpty()) {
-                        eventData.put(MessageConstants.MSG_HEADER_TOKEN, sessionToken);
-                    } else {
-                        LOGGER.warn("Invalid session token received");
-                    }
-                    eb.publish(MessagebusEndpoints.MBEP_EVENT, eventData);
-                }
-            });
-
-        }).completionHandler(result -> {
-            if (result.succeeded()) {
-                LOGGER.info("Profile end point ready to listen");
-            } else {
-                LOGGER.error("Error registering the profile handler. Halting the Profile machinery");
-                Runtime.getRuntime().halt(1);
-            }
-        });
     }
 
     @Override
