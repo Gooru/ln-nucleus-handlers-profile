46fd682810b6a8bf6f91361f7d3078670319816b
am-gooru <ashish@gooru.org>
2016-05-12 16:32:09 +0530

    Send the event instead of publishing it

--- src/main/java/org/gooru/nucleus/handlers/profiles/bootstrap/ProfileVerticle.java
+++ src/main/java/org/gooru/nucleus/handlers/profiles/bootstrap/ProfileVerticle.java
@@ -48,7 +48,7 @@ public class ProfileVerticle extends AbstractVerticle {
                             } else {
                                 LOGGER.warn("Invalid session token received");
                             }
-                            eb.publish(MessagebusEndpoints.MBEP_EVENT, eventData);
+                            eb.send(MessagebusEndpoints.MBEP_EVENT, eventData);
                         }
                     });
                 }).completionHandler(result -> {
